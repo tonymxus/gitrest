0d2bca7703e68b68028616a9501a77ba789911b6
uday <nelluri.uday@gmail.com>
2016-09-17 21:33:03 +0530

    ok sir
    ok sir

---
+++ GitRest/GitRest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitRest
+//
+//  Created by Uday Kiran on 17/09/16.
+//  Copyright © 2016 Uday Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
